07a7f2b09d53b1137a693026bc111db9901ca971
Eirlys Vo <voquyphiqn2004@gmail.com>
2025-06-13 19:39:05 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,9 +16,11 @@ session = cnx.session()
 name_on_order = st.text_input('Name on Smoothie:')
 st.write('The name on your Smoothie will be:', name_on_order)
 
-my_dataframe = session.table('smoothies.public.fruit_options').select(col('FRUIT_NAME'))
-# st.dataframe(data = my_dataframe, use_container_width=True)
+my_dataframe = session.table('smoothies.public.fruit_options').select(col('FRUIT_NAME'), col('SEARCH_ON)
+st.dataframe(data =  my_dataframe, use_container_width = True)
+st.stop()
 
+                                                                                             
 from snowflake.snowpark.functions import col
 
 # 🥋 Add a Multiselect 
